c0e87bb5695df210b5db9b949dfe5455f4a61364
spradhuman <shrestha@veriteworks.co.jp>
2021-08-03 18:17:14 +0900

    git card modification

--- src/components/Card/Git/Card.js
+++ src/components/Card/Git/Card.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import './Card.scss'
+import React from "react";
+import "./Card.scss";
 
 function Card({ name, content, lang, url }) {
   return (
     <>
       <div className="git-box">
         <a className="git-link" href={url}>
-          <h2 className="git-title">{name}</h2>
+          <h2 className="git-title">{name.slice(0, 20) + ".."}</h2>
         </a>
-        <p className="git-content">
-          {content}
-        </p>
+        <p className="git-content">{content}</p>
         <h4 className="git-lang">{lang}</h4>
       </div>
     </>
   );
 }
 
-export default Card
+export default Card;
